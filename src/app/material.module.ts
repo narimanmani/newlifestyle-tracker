2eb6a9021be681802e25c7ef01c4ed48b247c7be
nariman_mani <nariman.mani@gmail.com>
2021-01-11 01:25:03 -0500

    update

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -14,6 +14,9 @@ import { MatCardModule } from "@angular/material/card";
 import { MatSelectModule } from "@angular/material/select";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatDialogModule } from "@angular/material/dialog";
+import { MatTableModule } from "@angular/material/table";
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 
 
@@ -34,7 +37,10 @@ import { MatDialogModule } from "@angular/material/dialog";
         MatCardModule,
         MatSelectModule,
         MatProgressSpinnerModule,
-        MatDialogModule
+        MatDialogModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule
     ],
     exports: [
         MatButtonModule,
@@ -51,7 +57,10 @@ import { MatDialogModule } from "@angular/material/dialog";
         MatCardModule,
         MatSelectModule,
         MatProgressSpinnerModule,
-        MatDialogModule
+        MatDialogModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule
     ]
 
 })
